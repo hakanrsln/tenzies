dbfe5a4a122a9a17cb8586fda0ce51187e85abfd
hakanrsln <hakanrsln06@gmail.com>
2022-06-26 22:10:36 +0300

    deploy static html-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
 import './App.css';
+import Die from './Die';
 
 function App() {
   return (
-   <main></main>
+   <main>
+    <div className="dice-container">
+
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="1" />
+        <Die value="3" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="5" />
+    </div>
+   </main>
   );
 }
 
