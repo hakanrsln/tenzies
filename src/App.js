d579f31ba924f590e8abb01eaa9a749263cf8d5a
hakanrsln <hakanrsln06@gmail.com>
2022-06-27 22:29:14 +0300

    adding picking random num function to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 
+import React from 'react';
 import './App.css';
 import Die from './Die';
 
 function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+
+    function allNewDice() {
+      // new array to hold my numbers
+      const newDice = []
+      //loop 10 times
+      for(let i=0; i<10; i++){
+        newDice.push(Math.ceil(Math.random() * 6))
+      }
+
+      return newDice
+        //push a random number from 1-6 to my array
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
   return (
    <main>
     <div className="dice-container">
 
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="1" />
-        <Die value="3" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="5" />
+        {diceElements}
+        
     </div>
    </main>
   );
