65e5ba40dd2a8797139ade8f5d0c7d719d11cc3e
hakanrsln <hakanrsln06@gmail.com>
2022-06-29 14:34:53 +0300

    Adding holdDice property to app

--- src/App.js
+++ src/App.js
@@ -6,27 +6,42 @@ import { nanoid } from "nanoid";
 
 function App() {
     const [dice, setDice] = React.useState(allNewDice())
-
+    
+    function generateNewDie() {
+      return {
+        value:Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id:nanoid()
+    }
+    }
     function allNewDice() {
       // new array to hold my numbers
       const newDice = []
       //loop 10 times
       for(let i=0; i<10; i++){
-        newDice.push({
-          value:Math.ceil(Math.random() * 6),
-        isHeld: true,
-        id:nanoid()
-      })
+        newDice.push(generateNewDie())
       }
 
       return newDice
         //push a random number from 1-6 to my array
     }
     function rollDice() {
-      setDice(allNewDice())
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+        die :
+        generateNewDie()
+      }))
     }
 
-    const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />)
+    function holdDice(id) {
+      setDice(oldDice => oldDice.map(die =>{
+        return die.id === id ?
+              {...die, isHeld: !die.isHeld} : 
+              die
+      }))
+    }
+
+    const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
 
 
